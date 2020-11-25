ecf52cde1032411c5e30bdf66aa8166dcacba0cd
pedro-hen-braga <73506802+pedro-hen-braga@users.noreply.github.com>
2020-11-25 20:58:11 -0300

    Update main.c

--- main.c
+++ main.c
@@ -38,8 +38,8 @@ void lerArquivo(t_arquivo* arquivo){
     fread(&arquivo->qnt_transacoes, sizeof(int), 1, doc);
 
     // Aloca a memória considerada adequada para categorias e transações
-    arquivo->categorias = calloc(sizeof(t_categoria), arquivo->qnt_categorias + 5);
-    arquivo->transacoes = calloc(sizeof(t_transacao), arquivo->qnt_transacoes + 20);
+    arquivo->categorias = calloc(sizeof(t_categoria), arquivo->qnt_categorias + 2);
+    arquivo->transacoes = calloc(sizeof(t_transacao), arquivo->qnt_transacoes + 2);
 
     // Lê categorias e transações
     fread(arquivo->categorias, sizeof(t_categoria), arquivo->qnt_categorias, doc);
@@ -64,6 +64,18 @@ void salvarArquivo(t_arquivo* arquivo){
 
     //Fecha o arquivo
     fclose(doc);
+}
+
+void imprimeTransacao(t_transacao transacao, int id, t_arquivo* arquivo){
+    if(id != 0){
+        printf("ID:           %05d\n", id);
+    }
+    transacao.tipo == 1 ? printf("Tipo:         credito\n") : printf("Tipo:         debito\n");
+    printf("Data:         %02d/%02d/%02d\n", transacao.data.dia, transacao.data.mes, transacao.data.ano);
+    printf("Descricao:    %s", transacao.descricao);
+    printf("Categoria:    %s\n", arquivo->categorias[transacao.categoria]);
+    printf("Valor:        %.2f\n", transacao.valor);
+    printf("\n");
 }
 
 t_data trataData(char* entrada){
@@ -102,23 +114,23 @@ void tomaTipo(t_transacao* transacao){
     fflush(stdin);
 }
 
-void tomaData(t_transacao* transacao){
-    t_data data = {0, 0, 0};
+void tomaData(t_data* data){
+    t_data data_aux = {0, 0, 0};
     char entrada[11];
-    while(data.dia == 0){
+    while(data_aux.dia == 0){
         printf("Formato de entrada: DD/MM/AAAA\n");
         printf("Data: ");
         fflush(stdin);
         fgets(entrada, 11, stdin);
-        data = trataData(entrada);
+        data_aux = trataData(entrada);
         printf("\n");
     }
     fflush(stdin);
-    transacao->data = data;
+    *data = data_aux;
 }
 
 void tomaDescricao(t_transacao* transacao){
-    char entrada[101];
+    char entrada[101] = " ";
     printf("Maximo de 100 caracteres\n");
     printf("Descricao: ");
     fflush(stdin);
@@ -128,14 +140,18 @@ void tomaDescricao(t_transacao* transacao){
     printf("\n");
 }
 
+void imprimeCategorias(t_arquivo* arquivo){
+    for(int i = 0; i < arquivo->qnt_categorias; i++){
+        printf("%d. %s\n", i+1, arquivo->categorias[i]);
+    }
+}
+
 void tomaCategoria(t_transacao* transacao, t_arquivo* arquivo){
     char entrada[2];
     int categoria = -1;
     while(categoria < 0 || categoria >= arquivo->qnt_categorias){
         printf("Digite o numero da categoria\n");
-        for(int i = 0; i < arquivo->qnt_categorias; i++){
-            printf("%d. %s\n", i+1, arquivo->categorias[i]);
-        }
+        imprimeCategorias(arquivo);
         printf("Categoria: ");
         fflush(stdin);
         fgets(entrada, 2, stdin);
@@ -156,28 +172,21 @@ void tomaValor(t_transacao* transacao){
     transacao->valor = entrada;
 }
 
-void apresentaTransacao(t_transacao* transacao, t_arquivo* arquivo){
-    printf("Resumo da transacao:\n");
-    printf("Data: %02d/%02d/%04d\n", transacao->data.dia, transacao->data.mes, transacao->data.ano);
-    transacao->tipo == 1 ? printf("Operacao de credito\n") : printf("Operacao de debito\n");
-    printf("Descricao: %s", transacao->descricao);
-    printf("Categoria: %s\n", arquivo->categorias[transacao->categoria]);
-    printf("Valor: R$%.2f\n", transacao->valor);
-    printf("\n");
-}
-
 void incluiTransacao(t_transacao* transacao, t_arquivo* arquivo){
+    //Insere a transacao no arquivo
     transacao->valor *= transacao->tipo;
     arquivo->transacoes[arquivo->qnt_transacoes] = *transacao;
     arquivo->qnt_transacoes++;
     arquivo->saldo += transacao->valor;
-    salvarArquivo(arquivo);
+    //Realoca memória para o vetor transacoes
+    arquivo->transacoes = realloc(arquivo->transacoes, (arquivo->qnt_transacoes + 2) * sizeof(t_transacao));
     printf("Transacao adicionada com sucesso!\n\n");
     fflush(stdin);
 }
 
 //Declaração da função confirmaTransação() para resolver o warning de função não declarada
 void confirmaTransacao();
+void ordenaTransacoes();
 
 void editaTransacao(t_transacao* transacao, t_arquivo* arquivo){
     int entrada = 0;
@@ -197,7 +206,7 @@ void editaTransacao(t_transacao* transacao, t_arquivo* arquivo){
             tomaTipo(transacao);
             break;
         case 2:
-            tomaData(transacao);
+            tomaData(&transacao->data);
             break;
         case 3:
             tomaDescricao(transacao);
@@ -214,7 +223,7 @@ void editaTransacao(t_transacao* transacao, t_arquivo* arquivo){
             break;
         }
     }
-    apresentaTransacao(transacao, arquivo);
+    imprimeTransacao(*transacao, 0, arquivo);
     confirmaTransacao(transacao, arquivo);
 }
 
@@ -247,12 +256,14 @@ void confirmaTransacao(t_transacao* transacao, t_arquivo* arquivo){
 void criarTransacao(t_arquivo* arquivo){
     t_transacao transacao;
     tomaTipo(&transacao);
-    tomaData(&transacao);
+    tomaData(&transacao.data);
     tomaDescricao(&transacao);
     tomaCategoria(&transacao, arquivo);
     tomaValor(&transacao);
-    apresentaTransacao(&transacao, arquivo);
+    imprimeTransacao(transacao, 0, arquivo);
     confirmaTransacao(&transacao, arquivo);
+    ordenaTransacoes(arquivo);
+    salvarArquivo(arquivo);
 }
 
 void trocaTransacoes(t_arquivo* arquivo, int j){
@@ -261,23 +272,23 @@ void trocaTransacoes(t_arquivo* arquivo, int j){
     arquivo->transacoes[j+1] = temp;
 }
 
-int comparaDatasTransacoes(t_transacao t1, t_transacao t2){
-    //t1 mais recente que t2    +1
-    //t1 mesma data que t2      0
-    //t1 mais antigo que t2     -1
-    if(t1.data.ano > t2.data.ano){
+int comparaDatas(t_data data1, t_data data2){
+    //data1 mais recente que data2    +1
+    //data1 mesma data que data2      0
+    //data1 mais antigo que data2     -1
+    if(data1.ano > data2.ano){
         return 1;
-    }else if(t1.data.ano < t2.data.ano){
+    }else if(data1.ano < data2.ano){
         return -1;
     }else{
-        if(t1.data.mes > t2.data.mes){
+        if(data1.mes > data2.mes){
             return 1;
-        }else if(t1.data.mes < t2.data.mes){
+        }else if(data1.mes < data2.mes){
             return -1;
         }else{
-            if(t1.data.dia > t2.data.dia){
+            if(data1.dia > data2.dia){
                 return 1;
-            }else if(t1.data.dia < t2.data.dia){
+            }else if(data1.dia < data2.dia){
                 return -1;
             }else{
                 return 0;
@@ -287,14 +298,188 @@ int comparaDatasTransacoes(t_transacao t1, t_transacao t2){
 }
 
 void ordenaTransacoes(t_arquivo* arquivo){
-    for(int i = 0; i < arquivo->qnt_transacoes; i++){
-        for(int j = 0; j < arquivo->qnt_transacoes-1; j++){
-            if(comparaDatasTransacoes(arquivo->transacoes[j], arquivo->transacoes[j+1]) == 1){
-                trocaTransacoes(arquivo, j);
+    int continuar = 1;
+    while(continuar == 1){
+        continuar = 0;
+        for(int j = arquivo->qnt_transacoes-1; j > 0 ; j--){
+            if(comparaDatas(arquivo->transacoes[j-1].data, arquivo->transacoes[j].data) == 1){
+                trocaTransacoes(arquivo, j-1);
+                continuar = 1;
             }
         }
     }
     salvarArquivo(arquivo);
+}
+
+void imprimeTransacoes(t_transacao* transacoes, int qnt_transacoes, t_arquivo* arquivo){
+    for(int i = 0; i < qnt_transacoes; i++){
+        imprimeTransacao(transacoes[i], i+1, arquivo);
+    }
+}
+
+void tomaFiltroDatas(t_data* data_inicial, t_data* data_final){
+    printf("Indique a data incial do periodo desejado:\n");
+    tomaData(data_inicial);
+    printf("Indique a data final do periodo desejado:\n");
+    tomaData(data_final);
+}
+
+int verificaData(t_data* data_inicial, t_data* data_final, t_data* data){
+    if(comparaDatas(*data, *data_inicial) >= 0 && comparaDatas(*data, *data_final) <= 0){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int filtraTransacoes(t_transacao** transacoes, int** id_transacoes, int categoria, t_data data_i, t_data data_f, int tipo, t_arquivo* arquivo){
+    int qnt_transacoes = 0;
+    if(categoria != -1 && tipo != 0){
+        for(int i = 0; i < arquivo->qnt_transacoes; i++){
+            if(verificaData(&data_i, &data_f, &arquivo->transacoes[i].data) &&
+            arquivo->transacoes[i].categoria == categoria &&
+            arquivo->transacoes[i].tipo == tipo){
+                qnt_transacoes++;
+            }
+        }
+        *transacoes = calloc(sizeof(t_transacao), qnt_transacoes);
+        *id_transacoes = calloc(sizeof(int), qnt_transacoes);
+        int j = 0;
+        for(int i = 0; i < arquivo->qnt_transacoes; i++){
+            if(verificaData(&data_i, &data_f, &arquivo->transacoes[i].data) &&
+            arquivo->transacoes[i].categoria == categoria &&
+            arquivo->transacoes[i].tipo == tipo){
+                (*transacoes)[j] = arquivo->transacoes[i];
+                (*id_transacoes)[j] = i+1;
+                j++;
+            }
+        }
+    }else if(categoria != -1){
+        for(int i = 0; i < arquivo->qnt_transacoes; i++){
+            if(verificaData(&data_i, &data_f, &arquivo->transacoes[i].data) &&
+            arquivo->transacoes[i].categoria == categoria){
+                qnt_transacoes++;
+            }
+        }
+        *transacoes = calloc(sizeof(t_transacao), qnt_transacoes);
+        *id_transacoes = calloc(sizeof(int), qnt_transacoes);
+        int j = 0;
+        for(int i = 0; i < arquivo->qnt_transacoes; i++){
+            if(verificaData(&data_i, &data_f, &arquivo->transacoes[i].data) &&
+            arquivo->transacoes[i].categoria == categoria){
+                (*transacoes)[j] = arquivo->transacoes[i];
+                (*id_transacoes)[j] = i+1;
+                j++;
+            }
+        }
+    }else if(tipo != 0){
+        for(int i = 0; i < arquivo->qnt_transacoes; i++){
+            if(verificaData(&data_i, &data_f, &arquivo->transacoes[i].data) &&
+            arquivo->transacoes[i].tipo == tipo){
+                qnt_transacoes++;
+            }
+        }
+        *transacoes = calloc(sizeof(t_transacao), qnt_transacoes);
+        *id_transacoes = calloc(sizeof(int), qnt_transacoes);
+        int j = 0;
+        for(int i = 0; i < arquivo->qnt_transacoes; i++){
+            if(verificaData(&data_i, &data_f, &arquivo->transacoes[i].data) &&
+            arquivo->transacoes[i].tipo == tipo){
+                (*transacoes)[j] = arquivo->transacoes[i];
+                (*id_transacoes)[j] = i+1;
+                j++;
+            }
+        }
+    }else{
+        for(int i = 0; i < arquivo->qnt_transacoes; i++){
+            if(verificaData(&data_i, &data_f, &arquivo->transacoes[i].data)){
+                qnt_transacoes++;
+                printf("Entrou uma\n");
+            }
+        }
+        *transacoes = calloc(sizeof(t_transacao), qnt_transacoes);
+        *id_transacoes = calloc(sizeof(int), qnt_transacoes);
+        int j = 0;
+        for(int i = 0; i < arquivo->qnt_transacoes; i++){
+            if(verificaData(&data_i, &data_f, &arquivo->transacoes[i].data)){
+                (*transacoes)[j] = arquivo->transacoes[i];
+                (*id_transacoes)[j] = i+1;
+                j++;
+            }
+        }
+    }
+    return qnt_transacoes;
+}
+
+void vizualizarTransacoes(t_arquivo* arquivo){
+    // define datas iniciais e finais
+    t_data data_i = {1, 1, 0};
+    t_data data_f = {31, 12, 3000};
+    int entrada = 0;
+    while(entrada < 1 || entrada > 2){
+        printf("Voce deseja definir um filtro de data?\n");
+        printf("1. Sim\n");
+        printf("2. Nao\n");
+        printf("Insira a opcao desejada: ");
+        fflush(stdin);
+        scanf("%d", &entrada);
+        printf("\n");
+    }
+    if(entrada == 1){
+        tomaFiltroDatas(&data_i, &data_f);
+    }
+
+    // define filtro de categoria
+    int categoria = -1;
+    entrada = -1;
+    while(entrada < 0 || entrada > arquivo->qnt_categorias){
+        printf("Voce gostaria de selecionar uma categoria especifica?\n");
+        printf("0. Nao selecionar categoria\n");
+        imprimeCategorias(arquivo);
+        printf("Insira a opcao desejada: ");
+        fflush(stdin);
+        scanf("%d", &entrada);
+        printf("\n");
+    }
+    categoria = entrada-1;
+
+    // define filtro de tipo
+    int tipo = 0;
+    entrada = -1;
+    while(entrada < 0 || entrada > 2){
+        printf("Voce gostaria de selecionar um tipo especifico de transacao?\n");
+        printf("0. Nao selecionar tipo\n");
+        printf("1. Credito\n");
+        printf("2. Debito\n");
+        printf("Insira a opcao desejada: ");
+        fflush(stdin);
+        scanf("%d", &entrada);
+        printf("\n");
+    }
+    if(entrada == 2){
+        tipo = -1;
+    }else{
+        tipo = entrada;
+    }
+
+    printf("Filtros criados\n");
+    printf("categoria: %d\n", categoria);
+    printf("Tipo: %d\n", tipo);
+
+    //Cria os ponteiros transacoes e id_transacoes
+    t_transacao* transacoes;
+    int* id_transacoes;
+
+    printf("Ponteiros criados\n");
+    //Chama a função filtraTransacoes()
+    int qnt_transacoes = filtraTransacoes(&transacoes, &id_transacoes, categoria, data_i, data_f, tipo, arquivo);
+    printf("%d transacoes filtradas\n", qnt_transacoes);
+
+    //Imprime as transacoes
+    for(int i = 0; i < qnt_transacoes; i++){
+        imprimeTransacao(transacoes[i], id_transacoes[i], arquivo);
+    }
+    printf("%d transacoes impressas\n", qnt_transacoes);
 }
 
 void atualizarTransacao(t_arquivo* arquivo){}
@@ -303,15 +488,24 @@ void gerarRelatorio(t_arquivo* arquivo){}
 void criarCategoria(t_arquivo* arquivo){}
 void editarCategoria(t_arquivo* arquivo){}
 
+void resetar(t_arquivo* arquivo){
+    t_categoria categorias[] = {"Trabalho", "Transporte", "Alimentacao", "Lazer", "Outros"};
+    arquivo->categorias = categorias;
+    arquivo->qnt_categorias = 5;
+    arquivo->qnt_transacoes = 0;
+    arquivo->saldo = 0;
+    salvarArquivo(arquivo);
+}
 
 int main(void){
     t_arquivo arquivo;
     lerArquivo(&arquivo);
-    printf("Saldo: %.2f\n\n", arquivo.saldo);
-    for(int i = 0; i < arquivo.qnt_transacoes; i++){
-        printf("Data: %02d/%02d/%04d\n", arquivo.transacoes[i].data.dia, arquivo.transacoes[i].data.mes, arquivo.transacoes[i].data.ano);
-        printf("Valor: %.2f\n\n", arquivo.transacoes[i].valor);
+    while(1){
+        printf("Saldo: %.2f\n\n", arquivo.saldo);
+        imprimeTransacoes(arquivo.transacoes, arquivo.qnt_transacoes, &arquivo);
+        atualizarTransacao(&arquivo);
     }
-    //ordenaTransacoes(&arquivo);
+    //vizualizarTransacoes(&arquivo);
+    //resetar(&arquivo);
 return 0;
 }
